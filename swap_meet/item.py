cb72f288383c33177f7d4b4cd88b85141fb6b2b2
Esther Kang <estherkang.j@gmail.com>
2025-04-02 13:28:35 -0700

    adds condition attribute, CONDITION_DESCRIPTION global constant, and condition_description instance method

--- swap_meet/item.py
+++ swap_meet/item.py
@@ -1,15 +1,30 @@
 import uuid
 
+CONDITION_DESCRIPTIONS = {
+    0: "Acceptable",
+    1: "Good",
+    2: "Very Good",
+    3: "Excellent",
+    4: "Like New",
+    5: "New"
+}
+
 
 class Item:
-    def __init__(self, id=None):
-        if id is None:
-            self.id = uuid.uuid4().int
-        else:
-            self.id = id
+    def __init__(self, id=None, condition=0.0):
+        self.id = uuid.uuid4().int if id is None else id
+        self.condition = condition
 
     def get_category(self):
         return self.__class__.__name__
 
     def __str__(self):
         return f"An object of type {self.get_category()} with id {self.id}."
+
+    def condition_description(self):
+        condition = self.condition // 1
+
+        if condition not in CONDITION_DESCRIPTIONS:
+            return "Condition Out of Range"
+
+        return CONDITION_DESCRIPTIONS[condition]
