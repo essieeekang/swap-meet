9f83cc32014dfd25b3bc905c9c5d731023a79f28
Esther Kang <estherkang.j@gmail.com>
2025-04-02 13:25:27 -0700

    implements Electronics class and instance methods

--- swap_meet/electronics.py
+++ swap_meet/electronics.py
@@ -1,2 +1,11 @@
-class Electronics:
-    pass
+from swap_meet.item import Item
+
+
+class Electronics(Item):
+
+    def __init__(self, id=None, condition=0, type="Unknown"):
+        super().__init__(id, condition)
+        self.type = type
+
+    def __str__(self):
+        return super().__str__() + f" This is a {self.type} device."
