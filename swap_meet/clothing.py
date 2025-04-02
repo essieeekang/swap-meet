b997c92c5d85ab9b1c2d443dc19b6064ac10759f
Esther Kang <estherkang.j@gmail.com>
2025-04-02 13:26:45 -0700

    refactors Decor and Clothing classes to use inheritance and super()

--- swap_meet/clothing.py
+++ swap_meet/clothing.py
@@ -2,12 +2,9 @@ from swap_meet.item import Item
 
 
 class Clothing(Item):
-    def __init__(self,id=None,fabric="Unknown"):
-       super().__init__(id = id)
-       self.fabric = fabric
+    def __init__(self, id=None, condition=0, fabric="Unknown"):
+        super().__init__(id, condition)
+        self.fabric = fabric
 
-    def get_category(self):
-        return "Clothing"
-    
     def __str__(self):
-        return f"An object of type Clothing with id {self.id}. It is made from {self.fabric} fabric."
+        return super().__str__() + f" It is made from {self.fabric} fabric."
