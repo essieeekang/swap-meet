9ebe0bce2d3ad36dae0b3b11f822c08a95596849
Esther Kang <estherkang.j@gmail.com>
2025-04-02 14:36:06 -0700

    implements get_by_category, get_best_by_category, and swap_best_by_category instance methods in Vendor class

--- swap_meet/vendor.py
+++ swap_meet/vendor.py
@@ -39,3 +39,29 @@ class Vendor:
                             self.inventory[0],
                             other_vendor.inventory[0]
                             )
+
+    def get_by_category(self, category):
+        items_in_category = []
+        for item in self.inventory:
+            if item.get_category() == category:
+                items_in_category.append(item)
+
+        return items_in_category
+
+    def get_best_by_category(self, category):
+        items = self.get_by_category(category)
+        try:
+            best_item = max(items, key=lambda item: item.condition)
+            return best_item
+        except ValueError:
+            return None
+
+    def swap_best_by_category(self, other_vendor, my_priority, their_priority):
+        my_best_item = self.get_best_by_category(their_priority)
+        their_best_item = other_vendor.get_best_by_category(my_priority)
+
+        if not my_best_item or not their_best_item:
+            return False
+
+        self.swap_items(other_vendor, my_best_item, their_best_item)
+        return True
